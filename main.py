b2e9dbba102e003a924071e508910071130ae54f
shakil-py <shakilhaquepy@gmail.com>
2022-12-05 23:13:33 +0600

    add

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("hello world")
+
+a  = int(input("Please input total members : "))
+for i in range(a):
+    Bordername=input("insert student name : ")
+    Border1["name"]=Bordername
+    mill  
