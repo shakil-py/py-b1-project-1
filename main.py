07786514703856b2239751ac22456eabb1ab5736
shakil-py <shakilhaquepy@gmail.com>
2022-12-06 23:50:54 +0600

    add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
-a  = int(input("Please input total members : "))
+a = int(input("Please input total members : "))
 for i in range(a):
-    Bordername=input("insert student name : ")
-    Border1["name"]=Bordername
-    mill  
+    Bordername = input("insert student name : ")
+    Border1["name"] = Bordername
+    mill
