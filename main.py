43f2be5699f9fa210282b826e3f4b9503fc08e8c
shakil-py <shakilhaquepy@gmail.com>
2022-12-07 18:40:35 +0600

    add

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+#------------Constant/updateable Value------------
+
 member_name=["Shakil","Babu","Sadik","Rayhan","Morsed"]
 food_list=["Beef","Chicken","fish"]
+
+#----------------------------------------------------------
+
+#--------Input Data---------------------------------------
+
 today_market_item=input("Please Enter Today Market item (like Chicken , Beef or fish) : ")
 member = int(input("Please Enter amount of today Members : "))
 food_cost = int(input("Pleasr enter food cost : "))
+
+#---------------------------------------------------------
+
+#------------------Oparetion of Data----------------------
+
