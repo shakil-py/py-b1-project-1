0293b76d5ae3304dad8534aa66bca24746fa5fa3
shakil-py <shakilhaquepy@gmail.com>
2022-12-07 12:45:25 +0600

    ADD

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+food_cost_list={}
 member=int(input("Please Enter amount of Members : "))
 food=input("Please Enter this week food list: ").split(" ")
+food_cost=int (input("Pleasr enter food cost : "))
+
 fd=list(food)
-food_cost=dict()
+food_cost=list(food_cost)
 print(fd)
