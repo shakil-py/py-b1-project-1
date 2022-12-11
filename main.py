ae1c9cce8af2c40e813ce716f8cac20663c6a151
shakil-py <shakilhaquepy@gmail.com>
2022-12-11 13:46:53 +0600

    add

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ food_list=["Beef","Chicken","fish"]
 
 #----------------------------------------------------------
 
-#-----------------------Input Data-------------------------
+#-----------------------Input Data funtion-------------------------
 
 today_market_item=input("Please Enter Today Market item (like Chicken , Beef or fish) : ")
 member = int(input("Please Enter amount of today Members : "))
@@ -13,5 +13,5 @@ market_cost = int(input("Pleasr enter food cost : "))
 
 #---------------------------------------------------------
 
-#------------------Oparetion of Data----------------------
+#------------------Oparetion of Data funtion----------------------
 
