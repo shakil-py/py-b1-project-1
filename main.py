14ba3dad22054707100bf949c1afa6c19b9b24e6
shakil-py <shakilhaquepy@gmail.com>
2022-12-11 13:45:35 +0600

    add

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ food_list=["Beef","Chicken","fish"]
 
 today_market_item=input("Please Enter Today Market item (like Chicken , Beef or fish) : ")
 member = int(input("Please Enter amount of today Members : "))
-food_cost = int(input("Pleasr enter food cost : "))
+market_cost = int(input("Pleasr enter food cost : "))
 
 #---------------------------------------------------------
 
