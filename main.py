de50c065010c996ee6a789adf5c425d70cfafd74
shakil-py <shakilhaquepy@gmail.com>
2022-12-22 01:38:22 +0600

    add

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+# -----------------------Input Data funtion-------------------------
+
+food_item = input(
+    "Please Enter Today Market item (like Chicken , Beef or fish) : ")
+member = int(input("Please Enter amount of today Members : "))
+market_cost = int(input("Pleasr enter foodMarket cost : "))
+# food_item = set(input("Enter Today Food item : "))
+
+# ---------------------------------------------------------
 # ------------Constant/updateable Value------------
 
 member_name = ["Shakil", "Babu", "Sadik", "Rayhan", "Morsed", "Ibrahim"]
-food_list = ["Beef", "Chicken", "fish"]
+food_list = ["beef", "chicken", "fish", "egg"]
+
 
 # ----------------------------------------------------------
-
-# -----------------------Input Data funtion-------------------------
-
-# today_market_item=input("Please Enter Today Market item (like Chicken , Beef or fish) : ")
-member = int(input("Please Enter amount of today Members : "))
-market_cost = int(input("Pleasr enter foodMarket cost : "))
-
-# ---------------------------------------------------------
-
 # ------------------Oparetion of Data funtion----------------------
-# ---- Oparetion funtion
+# --------------------Other Funtion-----------------------------
+
+
+def foodcycle():
+    for i in range(len(food_list)):
+        if food_list[i] == food_item:
+            next_item = print("Next Market item is "+ food_list[i+1])
+            return next_item
+
+ # ---- Oparetion funtion
 
 
 def millrate():
@@ -26,20 +36,24 @@ def millrate():
 
 def alart():
     if millrate > 20:
-        print("Maneger your millrate is high to Previouse day :( !!!")
+        str1 = print("Maneger your millrate is high to Previouse day :( !!!")
+        return str1
     elif millrate <= 20:
-        print("Great job Maneger Continue Your manegment :)!")
+        str2 = print("Great job Maneger Continue Your manegment :)!")
+        return str2
     else:
         pass
 
 
 # --- Main App Funtion -----
-millrate= millrate()
+millrate = millrate()
 alart = alart()
+foodcycle = foodcycle()
 
 
 def mainapp():
-    main= print(millrate, alart)
+    main = print(millrate, alart, foodcycle)
     return main
+
 
 mainapp()
